56dcd6b66170882453ebe88ee1aaa012ecc2f1b6
Kyumin Kim <math4tots@gmail.com>
2016-10-24 19:33:20 -0700

    lexer

--- foo.cc
+++ foo.cc
@@ -1,5 +1,6 @@
 // g++ foo.cc src/rock.cc --std=c++11 -Iinclude -Wall -Werror -Wpedantic && ./a.out
 #include <iostream>
+#include <cassert>
 #include "rock.hh"
 using namespace std;
 using namespace rock;
@@ -7,5 +8,24 @@ using namespace rock;
 int main() {
   Ast *literal = new Literal(nullptr, new String("Foo"));
   Result result = literal->eval(nullptr);
-  cout << (result.type == NORMAL) << endl;
+  assert(result.type == NORMAL);
+
+  std::vector<Token*> tokens = lex(new File("<test>", "'a' b class 4.1 3 ."));
+  assert(tokens.size() == 7);
+  assert(tokens[0]->type == "STRING");
+  assert(tokens[0]->value == "a");
+  assert(tokens[1]->type == "NAME");
+  assert(tokens[1]->value == "b");
+  assert(tokens[2]->type == "class");
+  assert(tokens[2]->value == "");
+  assert(tokens[3]->type == "NUMBER");
+  assert(tokens[3]->value == "4.1");
+  assert(tokens[4]->type == "NUMBER");
+  assert(tokens[4]->value == "3");
+  assert(tokens[5]->type == ".");
+  assert(tokens[5]->value == "");
+  assert(tokens[6]->type == "EOF");
+  assert(tokens[6]->value == "");
+
+  cout << "Tests pass!" << endl;
 }
--- include/rock.hh
+++ include/rock.hh
@@ -1,8 +1,11 @@
 #ifndef rock_hh
 #define rock_hh
 
+#include <algorithm>
+#include <cctype>
 #include <functional>
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -18,7 +21,10 @@ struct File {
 struct Token {
   File *const file;
   const long position;
-  Token(File *const f, long p): file(f), position(p) {}
+  const std::string type, value;
+  Token(File *const f, long p, const std::string& t): Token(f, p, t, "") {}
+  Token(File *const f, long p, const std::string& t, const std::string& v):
+      file(f), position(p), type(t), value(v) {}
 };
 
 //** stack
@@ -199,6 +205,171 @@ struct Name final: Ast {
   Name(Token *t, const std::string& n): Ast(t), name(n) {}
   Result eval(Scope *scope) const noexcept { return scope->get(name); }
 };
+
+//** lex
+
+struct Lexer final {
+  File *const file;
+  long position;
+  Token *last;
+  bool doneFlag;
+  Lexer(File *f): file(f), position(0), last(nullptr), doneFlag(false) {
+    last = extract();
+  }
+
+  Token *peek() const {
+    return last;
+  }
+
+  Token *next() {
+    if (last->type == "ERR" || last->type == "EOF") {
+      doneFlag = true;
+      return last;
+    } else {
+      Token *token = last;
+      last = extract();
+      return token;
+    }
+  }
+
+  bool done() const {
+    return doneFlag;
+  }
+
+private:
+  static bool isword(char c) {
+    return c == '_' || std::isalnum(c);
+  }
+  std::string slice(long start, long end) const {
+    return file->contents.substr(start, end - start);
+  }
+  char ch() const {
+    return ch(0);
+  }
+  char ch(long diff) const {
+    long p = position + diff;
+    return (p < 0 || p >= file->contents.size()) ? '\0' : file->contents[p];
+  }
+  bool startsWith(const std::string& prefix) const {
+    return startsWith(prefix, 0);
+  }
+  bool startsWith(const std::string& prefix, long diff) const {
+    for (char c: prefix) {
+      if (ch(diff++) != c) {
+        return false;
+      }
+    }
+    return true;
+  }
+  void skipWhitespace() {
+    while (std::isspace(ch())) {
+      position++;
+    }
+  }
+  Token *extract() {
+    std::set<std::string> keywords({
+      "class", "for", "return",
+    });
+    std::vector<std::string> symbols({
+      "(", ")", "[", "]", "{", "}", ";", "\\", ".",
+    });
+    std::sort(symbols.begin(), symbols.end());
+    std::reverse(symbols.begin(), symbols.end());
+
+    skipWhitespace();
+
+    if (ch() == '\0') {
+      return new Token(file, position, "EOF");
+    }
+
+    const long p = position;
+
+    // STRING
+    if (startsWith("r'") || startsWith("'") ||
+        startsWith("r\"") || startsWith("\"")) {
+      bool raw = false;
+      if (startsWith("r")) {
+        raw = true;
+        position++;
+      }
+      std::string quote = slice(position, position+1);
+      if (startsWith(quote + quote + quote)) {
+        quote = slice(position, position + 3);
+      }
+      position += quote.size();
+      std::string value;
+      while (!startsWith(quote)) {
+        if (ch() == '\0') {
+          return new Token(file, position, "ERR", "Unfinished string");
+        }
+        if (!raw && ch() == '\\') {
+          position++;
+          switch (ch()) {
+          case 'n': value += '\n'; break;
+          case 't': value += '\t'; break;
+          case '\'': value += '\''; break;
+          case '\"': value += '\"'; break;
+          default:
+            return new Token(file, position, "ERR", "Invalid escape");
+          }
+          position++;
+        } else {
+          value += ch();
+          position++;
+        }
+      }
+      position += quote.size();
+      return new Token(file, p, "STRING", value);
+    }
+
+    // SYMBOL
+    for (const auto& symbol: symbols) {
+      if (startsWith(symbol)) {
+        position += symbol.size();
+        return new Token(file, p, symbol);
+      }
+    }
+
+    // NUMBER
+    if (std::isdigit(ch()) || (ch() == '.' && std::isdigit(ch(1)))) {
+      while (std::isdigit(ch())) {
+        position++;
+      }
+      if (ch() == '.') {
+        position++;
+        while (std::isdigit(ch())) {
+          position++;
+        }
+      }
+      return new Token(file, p, "NUMBER", slice(p, position));
+    }
+
+    // NAME/keywords
+    while (isword(ch())) {
+      position++;
+    }
+    if (p != position) {
+      auto word = slice(p, position);
+      if (keywords.find(word) == keywords.end()) {
+        return new Token(file, p, "NAME", word);
+      } else {
+        return new Token(file, p, word);
+      }
+    }
+
+    // Unrecognized token
+    return new Token(file, position, "ERR", "Unrecognized token");
+  }
+};
+
+inline std::vector<Token*> lex(File *f) {
+  std::vector<Token*> tokens;
+  Lexer lexer(f);
+  while (!lexer.done()) {
+    tokens.push_back(lexer.next());
+  }
+  return tokens;
+}
 
 }
 
