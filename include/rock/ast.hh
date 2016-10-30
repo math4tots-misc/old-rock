c561dae8631705f9401e7e94ba4c8ed35981545b
Kyumin Kim <math4tots@gmail.com>
2016-10-29 20:01:14 -0700

    BROKEN WIP

--- include/rock/ast.hh
+++ include/rock/ast.hh
@@ -25,6 +25,40 @@ public:
   Result eval(Scope&);
 };
 
+class Block final: public Ast {
+public:
+  const std::vector<std::unique_ptr<Ast>> expressions;
+  Block(const std::vector<std::unique_ptr<Ast>>&);
+  Result eval(Scope&);
+};
+
+class If final: public Ast {
+public:
+  const std::unique_ptr<Ast> condition, body, other;
+  If(std::unique_ptr<Ast> c, std::unique_ptr<Ast> b, std::unique_ptr<Ast> o);
+  Result eval(Scope&);
+};
+
+class Signature final {
+  const Token token;
+  const std::vector<std::string> argnames;
+  const std::vector<std::string> optargnames;
+  const std::string varargname;
+};
+
+class Arguments final {
+public:
+  const Token token;
+  std::vector<std::unique_ptr<Ast>> args;
+  std::unique_ptr<Ast> vararg; // TODO: support varargs and iterables.
+  Arguments(const Token&, const std::vector<std::unique_ptr<Ast>>&);
+  Arguments(
+      const Token&,
+      const std::vector<std::unique_ptr<Ast>>&,
+      std::unique_ptr<Ast>);
+  Result evalargs(Scope&, std::vector<Reference>&);
+};
+
 }
 
 #endif//rock_ast_hh
